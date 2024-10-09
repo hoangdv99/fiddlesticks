be55df936d930935d0cc4a81060ee3cf9ca41fc4
hoangdv99 <hoangdv257@gmail.com>
2024-10-09 23:38:57 +0900

    add apis

---
+++ internal/data/filters.go
@@ -0,0 +1,18 @@
+package data
+
+import "greenlight.hoangdv99/internal/validator"
+
+type Filters struct {
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	v.Check(f.Page > 0, "page", "must be greater than 0")
+	v.Check(f.Page <= 10_000_000, "page", "must be less than 10 mil")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
+	v.Check(f.PageSize <= 100, "page_size", "must be less than 100")
+	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
